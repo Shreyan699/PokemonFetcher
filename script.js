75d19527722b76176fe6d005b7536a0b97d8acb4
Shreyan699 <shreyanbiswas128@gmail.com>
2025-01-09 12:15:23 +0530

    Add event listener for Pokémon selection to update information

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ async function FetchAllPokemon(){
         }
     }
 
-    UpdateInformation()
+    document.getElementById("pokemons").addEventListener("change", UpdateInformation);
 }
 
 FetchAllPokemon();
